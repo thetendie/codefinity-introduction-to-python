bb1e18f9588998052b98224580f2772dae505233
thetendie <>
2026-03-17 02:25:19 +0000

    While Loops

---
+++ loops/while_loops/main.py
@@ -0,0 +1,8 @@
+start_number = 5
+countdown_values = []
+current = start_number
+
+while current >= 1:
+    countdown_values.append(current)
+    current -= 1
+print("Discount countdown complete!", countdown_values)
